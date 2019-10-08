088981199952baa56c1908571c1cba34657f32ca
jeff.zhu <jeff.zhu@gdsinoyue.com>
2019-10-08 11:59:25 +0800

    修改vgg

--- dog_cat_cnn/main.py
+++ dog_cat_cnn/main.py
@@ -1,4 +1,5 @@
-from models.resnet import resnet34,resnet101
-
-model = resnet101()
+from models.resnet import resnet34, resnet101
+from models.vggnet import vgg16, vgg16_bn
+from models.alexnet import alexnet
+model = alexnet()
 print(model)
--- dog_cat_cnn/models/vggnet.py
+++ dog_cat_cnn/models/vggnet.py
@@ -6,6 +6,7 @@ __all__ = [
     'VGG', 'vgg11', 'vgg11_bn', 'vgg13', 'vgg13_bn', 'vgg16', 'vgg16_bn',
     'vgg19_bn', 'vgg19',
 ]
+
 model_urls = {
     'vgg11': 'https://download.pytorch.org/models/vgg11-bbd30ac9.pth',
     'vgg13': 'https://download.pytorch.org/models/vgg13-c768596a.pth',
